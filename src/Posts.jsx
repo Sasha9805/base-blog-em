9be249bf5473016c1d5b65531d0e8d8ec08dac1c
Sasha Pimenov <pimenovs33@gmail.com>
2024-09-05 20:03:58 +0300

    Added handling errors and loading

--- src/Posts.jsx
+++ src/Posts.jsx
@@ -9,14 +9,22 @@ export function Posts() {
 	const [currentPage, setCurrentPage] = useState(0);
 	const [selectedPost, setSelectedPost] = useState(null);
 
-	// replace with useQuery
-	const { data } = useQuery({
+	const { data, isError, isPending, error } = useQuery({
 		queryKey: ["posts"],
 		queryFn: fetchPosts,
 	});
 
-	if (!data) {
-		return <div />;
+	if (isPending) {
+		return <h3>Loading ...</h3>;
+	}
+
+	if (isError) {
+		return (
+			<>
+				<h3>Oops, something went wrong</h3>
+				<p>{error.toString()}</p>
+			</>
+		);
 	}
 
 	return (
